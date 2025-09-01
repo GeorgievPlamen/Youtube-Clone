663447da752bf30d14fe390d37a1f80e429226bb
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-09-01 08:44:51 +0300

    Adding on select on categories

--- youtube-clone/src/modules/home/ui/sections/categories-section.tsx
+++ youtube-clone/src/modules/home/ui/sections/categories-section.tsx
@@ -4,18 +4,32 @@ import { ErrorBoundary } from "react-error-boundary";
 import { trpc } from "@/trpc/client";
 import { Suspense } from "react";
 import FilterCarousel from "@/components/filter-carousel/filter-carousel";
+import { useRouter } from "next/navigation";
 
 const CategoriesSectionSuspense = ({ categoryId }: { categoryId?: string }) => {
   const [categories] = trpc.categories.getMany.useSuspenseQuery();
+  const router = useRouter();
 
   const data = categories.map(({ name, id }) => ({
     value: id,
     label: name,
   }));
 
+  const onSelect = (value: string | null) => {
+    const url = new URL(window.location.href);
+
+    if (value) {
+      url.searchParams.set("categoryId", value);
+    } else {
+      url.searchParams.delete("categoryId");
+    }
+
+    router.push(url.toString());
+  };
+
   return (
     <FilterCarousel
-      onSelect={(x) => console.log(x)}
+      onSelect={onSelect}
       value={categoryId}
       data={data}
     />
