2f8a6465600685fe66deed6a60766fb9a5d1d265
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-09-04 08:07:33 +0300

    videos section and infinite loading

--- youtube-clone/src/modules/studio/ui/view/StudioView.tsx
+++ youtube-clone/src/modules/studio/ui/view/StudioView.tsx
@@ -1,10 +1,14 @@
-import VideosSection from "../section/VideosSection"
+import VideosSection from "../section/VideosSection";
 
 const StudioView = () => {
   return (
-    <div>
+    <div className="flex flex-col gap-y-6 pt-2.5">
+      <div className="px-4">
+        <h1 className="text-2xl font-bold">Channel content</h1>
+        <p className="text-xs text-muted-foreground"></p>
         <VideosSection />
+      </div>
     </div>
-  )
-}
-export default StudioView
+  );
+};
+export default StudioView;
